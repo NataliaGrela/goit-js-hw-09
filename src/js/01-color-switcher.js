e9c02fa4b0585d7c9b548d3f6406f87951d4b2a7
Natalia Grela <nataliagrela83@mail.com>
2023-03-08 21:35:04 +0100

    hw 9
    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,18 @@
+const startBtn = document.querySelector('button');
+const stopBtn = document.querySelector('button').nextElementSibling;
+let timerId = null;
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+startBtn.addEventListener("click", () => {
+ timerId = setInterval(() => {
+    const body = document.querySelector('body')
+    body.style.background = getRandomHexColor()
+  }, 1000);
+});
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+});
